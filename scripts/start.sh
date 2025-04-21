d42ce66680aaa8c751c3e3c5014d0968deae6c65
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-21 16:16:05 +0530

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Running start.sh..."
+
+cd /home/ec2-user/chat-app
+
+# Pull the latest image from ECR
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin <your-aws-account-id>.dkr.ecr.ap-south-1.amazonaws.com
+docker pull <your-aws-account-id>.dkr.ecr.ap-south-1.amazonaws.com/chat-app:latest
+
+# Start the app with docker-compose
+docker-compose up -d
