4a9d43b28cbe9fdd916d0e18b61fec70f3261517
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-21 16:18:31 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,4 @@
 echo "Running after_install.sh..."
 
 # Change ownership (optional, if files are owned by root)
-chown -R ubuntu:ubuntu /home/ec2-user/chat-app
+chown -R ubuntu:ubuntu /home/ubuntu/chat-app
