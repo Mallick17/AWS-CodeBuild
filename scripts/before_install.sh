3597dc745743254586cb5af36076c0e6850becf1
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-21 16:18:11 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 echo "Running before_install.sh..."
 
 # Stop old containers
-docker-compose -f /home/ec2-user/chat-app/docker-compose.yml down || true
+docker-compose -f /home/ubuntu/chat-app/docker-compose.yml down || true
 
 # Remove old images (optional cleanup)
 docker system prune -af || true
