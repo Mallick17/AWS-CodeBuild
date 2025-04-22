b8082debb253662cc7b0ab0573c6b4eb9bff5b93
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-22 09:10:50 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,16 +4,21 @@ set -e
 echo "Running start.sh..."
 
 # Move to app directory
-cd /home/ubuntu/chat-app
+cd /home/ubuntu/chat-app || exit 1
+
+# Full paths to binaries
+AWS_CLI="/usr/bin/aws"
+DOCKER="/usr/bin/docker"
+DOCKER_COMPOSE="/usr/local/bin/docker-compose"
 
 # Authenticate with ECR (non-interactive)
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 339713104321.dkr.ecr.ap-south-1.amazonaws.com
+$AWS_CLI ecr get-login-password --region ap-south-1 | $DOCKER login --username AWS --password-stdin 339713104321.dkr.ecr.ap-south-1.amazonaws.com
 
 # Pull the latest image
-docker pull 339713104321.dkr.ecr.ap-south-1.amazonaws.com/chat-app:latest
+$DOCKER pull 339713104321.dkr.ecr.ap-south-1.amazonaws.com/chat-app:latest
 
-# Shut down any running containers
-docker-compose down || true
+# Shut down existing containers (if any)
+$DOCKER_COMPOSE down || true
 
 # Start containers in detached mode
-docker-compose up -d
+$DOCKER_COMPOSE up -d
