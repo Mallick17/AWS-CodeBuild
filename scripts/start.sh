7b9b72fd803a2a171ca78fe3538b69e6fee43c21
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-04-22 09:02:01 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+set -e
+
 echo "Running start.sh..."
 
+# Move to app directory
 cd /home/ubuntu/chat-app
 
-# Pull the latest image from ECR
+# Authenticate with ECR (non-interactive)
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 339713104321.dkr.ecr.ap-south-1.amazonaws.com
+
+# Pull the latest image
 docker pull 339713104321.dkr.ecr.ap-south-1.amazonaws.com/chat-app:latest
 
-# Start the app with docker-compose
+# Shut down any running containers
+docker-compose down || true
+
+# Start containers in detached mode
 docker-compose up -d
